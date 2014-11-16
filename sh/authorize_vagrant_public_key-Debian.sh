06f3c03c108c7d08478f12b31d19b8524154db30
Chris O'Neil <chris.oneil@gmail.com>
2014-11-16 15:04:30 +0000

    Debian script for adding vagrant user public key
    There is only a 1 line difference in this script compared to the CentOS
    one, which is that you don't appear to be allowed to add the user
    directrly to the sudoers file on a Debian distro.

---
+++ sh/authorize_vagrant_public_key-Debian.sh
@@ -0,0 +1,6 @@
+echo -e "\nInstalling vagrant public key..."
+mkdir -m 0700 /home/vagrant/.ssh
+cd /home/vagrant/.ssh
+wget --no-check-certificate -O authorized_keys https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub
+chmod 0600 /home/vagrant/.ssh/authorized_keys
+chown -R vagrant.users /home/vagrant/.ssh
